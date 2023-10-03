1b5ba2b40d446cfc9e25bd31ba9d5465d296f72b
twosky0202 <leehaneul0202@naver.com>
2023-10-03 18:27:55 +0900

    0926 inRange, 영역 연산(컨볼루션 - 가우시안, 엠보싱)

--- ch3/3inrange.py
+++ ch3/3inrange.py
@@ -1,17 +1,18 @@
 import cv2 as cv
 
-# gray = cv.imread('soccer.jpg', cv.IMREAD_GRAYSCALE)
+# inRange
+# gray = cv.imread('soccer.jpg', cv.IMREAD_GRAYSCALE)  # gray 영상
 # cv.imshow('original - gray', gray)
 
-# gray_mask = cv.inRange(gray, 120,170)    # 120~170이면 white, 아니면 black
+# gray_mask = cv.inRange(gray, 120, 170)    # 두 값 제시. 120 ~ 170이면 white, 아니면 black
 # cv.imshow('inRange', gray_mask)
 
-img = cv.imread('soccer.jpg')
+img = cv.imread('soccer.jpg')  # color 영상
 cv.imshow('original', img)
 
-hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-red_mask = cv.inRange(hsv_img, (-10,50,50), (70,255,255)) # hsv에서 red의 범위
-img_red = cv.bitwise_and(img, img, mask=red_mask) # and 연산자
+hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)  # 사람이 느끼는 색상 범위를 표현할 때는 rgb보다는 명도와 채도가 있는 hsv로 표현
+red_mask = cv.inRange(hsv_img, (-10, 50, 50), (10, 255, 255))  # 통상적으로 hsv에서 red의 범위
+img_red = cv.bitwise_and(img, img, mask=red_mask)  # and 연산자. black(0)으로 되어있으면 0으로, white(1)로 되어있으면(빨간색 범위에 있는 픽셀) img 값 그대로 출력
 # A and B : if B=0, 0, else if B=1, (A=0 -> 0, A=1 -> 1)
 
 # cv.imshow('red detection', red_mask) # red 부분
--- ch3/3inrange_video.py
+++ ch3/3inrange_video.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import sys
 
+# inRange로 video에서 skin color 찾기
 # cap = cv.VideoCapture(0, cv.CAP_DSHOW)  # 동영상을 가져오는 클래스
 cap = cv.VideoCapture('face2.mp4')
 
@@ -14,11 +15,11 @@ while True:  # 무한루프로
         print('프레임 획득에 실패하여 루프를 나갑니다.')
         break
 
-    hsv_img = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-    red_mask = cv.inRange(hsv_img, (0, 10, 60), (20, 255, 255))  # hsv에서 skin color의 범위
-    img_red = cv.bitwise_and(frame, frame, mask=red_mask)  # and 연산자
+    hsv_frame = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    skin_color_mask = cv.inRange(hsv_frame, (0, 10, 60), (20, 255, 255))  # hsv에서 skin color의 범위
+    img_skin_color = cv.bitwise_and(frame, frame, mask=skin_color_mask)  # and 연산자
 
-    cv.imshow('skin color detection', img_red)
+    cv.imshow('skin color detection', img_skin_color)
 
     key = cv.waitKey(1)
     if key == ord('q'):
