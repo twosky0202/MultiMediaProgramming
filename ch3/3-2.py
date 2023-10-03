50815adeebaa59fddb65370fbe1db7c324495036
twosky0202 <leehaneul0202@naver.com>
2023-10-03 16:47:32 +0900

    0921 히스토그램, 오츄(Otsu) 알고리즘, 히스토그램 평활화

--- ch3/3-2.py
+++ ch3/3-2.py
@@ -1,14 +1,15 @@
 import cv2 as cv
 import matplotlib.pyplot as plt
 
-img=cv.imread('soccer.jpg')
-h=cv.calcHist([img],[2],None,[256],[0,256]) # 2번 채널인 R 채널에서 히스토그램 구함
-plt.plot(h,color='r',linewidth=1) # r : 빨간색
+# 히스토그램
+img = cv.imread('soccer.jpg')
+h = cv.calcHist([img], [2], None, [256], [0, 256])  # 2번 채널인 R 채널에서 히스토그램 구함
+plt.plot(h, color='r', linewidth=1)  # r : 빨간색, linestyle default type : 실선
 
-h=cv.calcHist([img],[1],None,[256],[0,256]) # 1번 채널인 G 채널에서 히스토그램 구함
-plt.plot(h,color='g',linewidth=2, linestyle='dotted')
+hg = cv.calcHist([img], [1], None, [256], [0, 256])  # 1번 채널인 G 채널에서 히스토그램 구함
+plt.plot(hg, color='g', linewidth=2, linestyle='dotted')  # 점선
 
-h=cv.calcHist([img],[0],None,[256],[0,256]) # 0번 채널인 B 채널에서 히스토그램 구함
-plt.plot(h,color='b',linewidth=3, linestyle='dashed')
+hb = cv.calcHist([img], [0], None, [256], [0, 256])  # 0번 채널인 B 채널에서 히스토그램 구함
+plt.plot(hb, color='b', linewidth=3, linestyle='dashed')  # 파선
 
 plt.show()
--- ch3/3-3.py
+++ ch3/3-3.py
@@ -1,15 +1,16 @@
 import cv2 as cv
 
-# img=cv.imread('soccer.jpg')
-gray=cv.imread('soccer.jpg', cv.IMREAD_GRAYSCALE)
-            
-# t,bin_img=cv.threshold(img[:,:,2],0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
-t,bin_img=cv.threshold(gray,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
-print('오츄 알고리즘이 찾은 최적 임곗값=',t)
+# 오츄(Otsu) 알고리즘
+# img = cv.imread('soccer.jpg')
+gray = cv.imread('soccer.jpg', cv.IMREAD_GRAYSCALE)
 
-# cv.imshow('R channel',img[:,:,2])			# R 채널 영상
-cv.imshow('Gray',gray)			# gray 영상
-cv.imshow('R channel binarization',bin_img)	# R 채널 이진화 영상
+# t, bin_img = cv.threshold(img[:, :, 2], 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+t, bin_img = cv.threshold(gray, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)  # 자동으로 적절한 임계값을 찾아서 그 임계값으로 이진화한 영상 리턴
+print('오츄 알고리즘이 찾은 최적 임곗값=', t)
+
+# cv.imshow('R channel', img[:, :, 2])  # R 채널 영상
+cv.imshow('Gray', gray)  # gray 영상
+cv.imshow('binarization', bin_img)  # 이진화 영상
 
 cv.waitKey()
 cv.destroyAllWindows()
