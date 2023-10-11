e333755f0b93d4e5d553d4052a7942be73252939
twosky0202 <leehaneul0202@naver.com>
2023-10-12 02:47:18 +0900

    0928 영역 연산 - 모폴로지(morphology), 연결 요소

--- ch3/3-4.py
+++ ch3/3-4.py
@@ -2,30 +2,43 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
-img=cv.imread('JohnHancocksSignature.png',cv.IMREAD_UNCHANGED) 
+# 모폴로지
+# img = cv.imread('JohnHancocksSignature.png', cv.IMREAD_UNCHANGED)
+img = cv.imread('morph.jpg', cv.IMREAD_GRAYSCALE)
 
-t,bin_img=cv.threshold(img[:,:,3],0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
-plt.imshow(bin_img,cmap='gray'), plt.xticks([]), plt.yticks([])
-plt.show() 
-
-b=bin_img[bin_img.shape[0]//2:bin_img.shape[0],0:bin_img.shape[0]//2+1]
-plt.imshow(b,cmap='gray'), plt.xticks([]), plt.yticks([])
+# t, bin_img = cv.threshold(img[:, :, 3], 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)  # 이진 영상
+t, b = cv.threshold(img, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+plt.imshow(b, cmap='gray'), plt.xticks([]), plt.yticks([])
 plt.show()
 
-se=np.uint8([[0,0,1,0,0],			# 구조 요소
-            [0,1,1,1,0],
-            [1,1,1,1,1],
-            [0,1,1,1,0],
-            [0,0,1,0,0]])
+# b = bin_img[bin_img.shape[0] // 2:bin_img.shape[0], 0:bin_img.shape[0] // 2 + 1]  # 일부만 잘라서 사용
+# plt.imshow(b, cmap='gray'), plt.xticks([]), plt.yticks([])
+# plt.show()
 
-b_dilation=cv.dilate(b,se,iterations=1)	# 팽창
-plt.imshow(b_dilation,cmap='gray'), plt.xticks([]), plt.yticks([])
-plt.show()
+se = np.uint8([[0, 0, 1, 0, 0],  # 구조 요소 포함 여부 언급 목적
+               [0, 1, 1, 1, 0],  # -> Convolution 연산 아님
+               [1, 1, 1, 1, 1],
+               [0, 1, 1, 1, 0],
+               [0, 0, 1, 0, 0]])
 
-b_erosion=cv.erode(b,se,iterations=1)	# 침식
-plt.imshow(b_erosion,cmap='gray'), plt.xticks([]), plt.yticks([])
-plt.show()
+b_dilation = cv.dilate(b, se, iterations=1)  # 팽창 (입력영상, 구조 요소, 반복 횟수)
+# plt.imshow(b_dilation, cmap='gray'), plt.xticks([]), plt.yticks([])
+# plt.show()
 
-b_closing=cv.erode(cv.dilate(b,se,iterations=1),se,iterations=1)	# 닫기
-plt.imshow(b_closing,cmap='gray'), plt.xticks([]), plt.yticks([])
-plt.show()
+b_erosion = cv.erode(b, se, iterations=1)  # 침식
+# plt.imshow(b_erosion, cmap='gray'), plt.xticks([]), plt.yticks([])
+# plt.show()
+
+b_closing = cv.erode(cv.dilate(b, se, iterations=1), se, iterations=1)  # 닫힘 : 팽창 -> 침식
+# plt.imshow(b_closing, cmap='gray'), plt.xticks([]), plt.yticks([])
+# plt.show()
+
+b_opening = cv.dilate(cv.erode(b, se, iterations=1), se, iterations=1)  # 열림 : 침식 -> 팽창
+# plt.imshow(b_opening, cmap='gray'), plt.xticks([]), plt.yticks([])
+# plt.show()
+
+morph = np.hstack((b, b_dilation, b_erosion, b_closing, b_opening))
+cv.imshow('morph', morph)
+
+cv.waitKey()
+cv.destroyAllWindows()
--- ch3/3-7.py
+++ ch3/3-7.py
@@ -6,13 +6,13 @@ img = cv.imread('soccer.jpg')
 img = cv.resize(img, dsize=(0, 0), fx=0.4, fy=0.4)
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.putText(gray, 'soccer', (10, 20), cv.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
-cv.imshow('Original', gray)
+# cv.imshow('Original', gray)
 
 # 가우시안 필터. 스무딩/블러링 필터
 smooth = np.hstack((cv.GaussianBlur(gray, (5, 5), 0.0), cv.GaussianBlur(gray, (9, 9), 0.0),
                     cv.GaussianBlur(gray, (15, 15),
                                     0.0)))  # 필터 크기(ex.(5, 5), (9, 9)) 제공해주면 GaussianBlur에서 계산을 해줌. 0.0 : 시그마값. 특별한 언급이 없는 한 0.0 넣어주기
-cv.imshow('Smooth', smooth)
+# cv.imshow('Smooth', smooth)
 
 # 엠보싱 필터
 femboss = np.array([[-1.0, 0.0, 0.0],
@@ -24,9 +24,45 @@ emboss = np.uint8(np.clip(cv.filter2D(gray16, -1, femboss) + 128, 0, 255))  # 0~
 emboss_bad = np.uint8(cv.filter2D(gray16, -1, femboss) + 128)
 emboss_worse = cv.filter2D(gray, -1, femboss)
 
-cv.imshow('Emboss', emboss)
-cv.imshow('Emboss_bad', emboss_bad)
-cv.imshow('Emboss_worse', emboss_worse)
+# cv.imshow('Emboss', emboss)
+# cv.imshow('Emboss_bad', emboss_bad)
+# cv.imshow('Emboss_worse', emboss_worse)
+
+# 평균값 필터
+faverage = np.array([[1.0/9.0, 1.0/9.0, 1.0/9.0],
+                    [1.0/9.0, 1.0/9.0, 1.0/9.0],
+                    [1.0/9.0, 1.0/9.0, 1.0/9.0]])
+
+# 샤프닝 필터 - 전체 합 0 -> 경계선만
+fsharpening1 = np.array([[0.0, -1.0, 0.0],
+                        [-1.0, 4.0, -1.0],
+                        [0.0, -1.0, 0.0]])
+
+# 샤프닝 필터 - 전체 합 1 -> 원래 이미지에 경계선 추가
+fsharpening2 = np.array([[0.0, -1.0, 0.0],
+                        [-1.0, 5.0, -1.0],
+                        [0.0, -1.0, 0.0]])
+
+result = cv.filter2D(gray, -1, faverage)
+# result = cv.filter2D(gray, -1, fsharpening1)
+# result = cv.filter2D(gray, -1, fsharpening2)
+# cv.imshow('result', result)
+
+gray = cv.imread('coins.png', cv.IMREAD_GRAYSCALE)
+
+# 평균값 필터 - 함수 사용
+average = cv.blur(gray, (9, 9))
+cv.imshow('result - average', average) # faverage와 똑같은 필터
+
+# 중간값 필터
+median = cv.medianBlur(gray, 3)
+cv.imshow('result - median', median)
+
+bilateral = cv.bilateralFilter(gray, -1, sigmaColor=5, sigmaSpace=5)
+# 각 픽셀과 주변 요소들로부터 가중 평균을 구함 => 가우시안과 유사
+# 단, 픽셀값의 차이도 같이 사용하여 유사한 픽셀에 더 큰 가중치를 두는 방법
+# 경계선을 유지하며 스무딩
+cv.imshow('result - bilateral', bilateral)
 
 cv.waitKey()
 cv.destroyAllWindows()
