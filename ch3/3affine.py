6c2f80a791431e893562c4d17287a130f7ce60a7
twosky0202 <leehaneul0202@naver.com>
2023-10-13 16:37:21 +0900

    1005 mosaic, 90도 회전, 에지 검출

--- ch3/3affine.py
+++ ch3/3affine.py
@@ -6,19 +6,24 @@ img = cv.imread('rose.png')
 rows, cols = img.shape[:2]  # shape은 영상의 크기(세로, 가로)
 
 # 좌우 대칭
-src_points = np.float32([[0, 0], [cols-1, 0], [0, rows-1]])
-dst_points = np.float32([[cols-1, 0], [0, 0], [cols-1, rows-1]])
+src_points = np.float32([[0, 0], [cols - 1, 0], [0, rows - 1]])
+dst_points = np.float32([[cols - 1, 0], [0, 0], [cols - 1, rows - 1]])
 
 # 상하 대칭
-src_points = np.float32([[0, 0], [cols-1, 0], [0, rows-1]])
-dst_points = np.float32([[0, rows-1], [cols-1, rows-1], [0, 0]])
+src_points = np.float32([[0, 0], [cols - 1, 0], [0, rows - 1]])
+dst_points = np.float32([[0, rows - 1], [cols - 1, rows - 1], [0, 0]])
 
 # 원점 대칭
-src_points = np.float32([[0, 0],  [0, rows-1], [cols-1, 0]])
-dst_points = np.float32([[cols-1, rows-1], [cols-1, 0], [0, rows-1]])
+src_points = np.float32([[0, 0], [0, rows - 1], [cols - 1, 0]])
+dst_points = np.float32([[cols - 1, rows - 1], [cols - 1, 0], [0, rows - 1]])
+
+# 90 반시계방향 rotate
+src_points = np.float32([[0, 0], [0, rows - 1], [cols - 1, 0]])
+dst_points = np.float32([[0, cols - 1], [rows - 1, cols - 1], [0, 0]])
 
 affine_matrix = cv.getAffineTransform(src_points, dst_points)
-img_symmetry = cv.warpAffine(img, affine_matrix, (cols, rows))  # 3번째 인자는 출력 영상의 크기(가로, 세로)
+# img_symmetry = cv.warpAffine(img, affine_matrix, (cols, rows))  # 3번째 인자는 출력 영상의 크기(가로, 세로)
+img_symmetry = cv.warpAffine(img, affine_matrix, (rows,cols))
 
 cv.imshow('Original', img)
 cv.imshow('Symmetry', img_symmetry)
--- ch4/4-1.py
+++ ch4/4-1.py
@@ -1,20 +1,29 @@
 import cv2 as cv
 
-img=cv.imread('soccer.jpg')
-gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+# 에지 연산자
+# img = cv.imread('soccer.jpg')
+img = cv.imread('check.png')
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-grad_x=cv.Sobel(gray,cv.CV_32F,1,0,ksize=3)	# 소벨 연산자 적용
-grad_y=cv.Sobel(gray,cv.CV_32F,0,1,ksize=3)
+gray = cv.blur(gray, (3, 3))  # 잡음 제거 효과
 
-sobel_x=cv.convertScaleAbs(grad_x)	# 절대값을 취해 양수 영상으로 변환
-sobel_y=cv.convertScaleAbs(grad_y)
+# 소벨 연산자 적용
+grad_x = cv.Sobel(gray, cv.CV_32F, 1, 0, ksize=3)  # gray 이미지만 가능. 음수 포함 위해 32비트로 바꿔줌. 3, 4번째 인자 1, 0 -> sobel_x
+grad_y = cv.Sobel(gray, cv.CV_32F, 0, 1, ksize=3)  # 3, 4번째 인자 0, 1 -> sobel_y
+grad = cv.Laplacian(gray, cv.CV_32F)
 
-edge_strength=cv.addWeighted(sobel_x,0.5,sobel_y,0.5,0)	# 에지 강도 계산
+# sobel_x = cv.convertScaleAbs(grad_x)  # 행렬의 음수 값을 절대값을 취해 양수 영상으로 변환
+# sobel_y = cv.convertScaleAbs(grad_y)
+lap = cv.convertScaleAbs(grad)
 
-cv.imshow('Original',gray)
-cv.imshow('sobelx',sobel_x)
-cv.imshow('sobely',sobel_y)
-cv.imshow('edge strength',edge_strength)
+# edge_strength = cv.addWeighted(sobel_x, 0.5, sobel_y, 0.5, 0)  # 에지 강도 계산. 두 비율의 합이 1이 되게
+# 0.5 * sobel_x + 0.5 * sobel_y + 0.0
+
+cv.imshow('Original', gray)
+# cv.imshow('sobelx', sobel_x)
+# cv.imshow('sobely', sobel_y)
+# cv.imshow('edge strength', edge_strength)
+cv.imshow('laplacian', lap)
 
 cv.waitKey()
 cv.destroyAllWindows()
