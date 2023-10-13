ec4edd0a0effe15ef347aeaa1b16f2acad32a7ad
twosky0202 <leehaneul0202@naver.com>
2023-10-13 16:24:04 +0900

    1003 기하 연산 - 보간 이용(resize), affine 변환

--- ch3/3-8.py
+++ ch3/3-8.py
@@ -1,17 +1,17 @@
 import cv2 as cv
 
-img=cv.imread('rose.png')
-patch=img[250:350,170:270,:]
+img = cv.imread('rose.png')
+patch = img[250:350, 170:270, :]  # 일부만 자름 -> 5배씩 확대
 
-img=cv.rectangle(img,(170,250),(270,350),(255,0,0),3)
-patch1=cv.resize(patch,dsize=(0,0),fx=5,fy=5,interpolation=cv.INTER_NEAREST)
-patch2=cv.resize(patch,dsize=(0,0),fx=5,fy=5,interpolation=cv.INTER_LINEAR)
-patch3=cv.resize(patch,dsize=(0,0),fx=5,fy=5,interpolation=cv.INTER_CUBIC)
+img = cv.rectangle(img, (170, 250), (270, 350), (255, 0, 0), 3)
+patch1 = cv.resize(patch, dsize=(0, 0), fx=11, fy=11, interpolation=cv.INTER_NEAREST)  # nearest neighbor 방법
+patch2 = cv.resize(patch, dsize=(0, 0), fx=11, fy=11, interpolation=cv.INTER_LINEAR)  # 양선형 보간법
+patch3 = cv.resize(patch, dsize=(0, 0), fx=11, fy=11, interpolation=cv.INTER_CUBIC)  # 3차 회선 보간법
 
-cv.imshow('Original',img)
-cv.imshow('Resize nearest',patch1) 
-cv.imshow('Resize bilinear',patch2) 
-cv.imshow('Resize bicubic',patch3) 
+cv.imshow('Original', img)
+cv.imshow('Resize nearest', patch1)
+cv.imshow('Resize bilinear', patch2)
+cv.imshow('Resize bicubic', patch3)
 
 cv.waitKey()
 cv.destroyAllWindows()
---
+++ ch3/3affine.py
@@ -0,0 +1,27 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('rose.png')
+
+rows, cols = img.shape[:2]  # shape은 영상의 크기(세로, 가로)
+
+# 좌우 대칭
+src_points = np.float32([[0, 0], [cols-1, 0], [0, rows-1]])
+dst_points = np.float32([[cols-1, 0], [0, 0], [cols-1, rows-1]])
+
+# 상하 대칭
+src_points = np.float32([[0, 0], [cols-1, 0], [0, rows-1]])
+dst_points = np.float32([[0, rows-1], [cols-1, rows-1], [0, 0]])
+
+# 원점 대칭
+src_points = np.float32([[0, 0],  [0, rows-1], [cols-1, 0]])
+dst_points = np.float32([[cols-1, rows-1], [cols-1, 0], [0, rows-1]])
+
+affine_matrix = cv.getAffineTransform(src_points, dst_points)
+img_symmetry = cv.warpAffine(img, affine_matrix, (cols, rows))  # 3번째 인자는 출력 영상의 크기(가로, 세로)
+
+cv.imshow('Original', img)
+cv.imshow('Symmetry', img_symmetry)
+
+cv.waitKey()
+cv.destroyAllWindows()
