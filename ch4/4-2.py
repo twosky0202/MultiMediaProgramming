a5c7a6ce25678c6d6879a593d75164c3b0bffd39
twosky0202 <leehaneul0202@naver.com>
2023-10-19 20:13:33 +0900

    1010 hw1, Canny 에지, 경계선 찾기(Contours), 허프 변환 - 원 검출

--- ch4/4-2.py
+++ ch4/4-2.py
@@ -1,15 +1,17 @@
 import cv2 as cv
 
-img=cv.imread('soccer.jpg')	# 영상 읽기
+# Canny 에지
+# img = cv.imread('soccer.jpg')  # 영상 읽기
+img = cv.imread('check.png')
 
-gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)  # Canny 알고리즘 적용 위해 영상 gray 처리
 
-canny1=cv.Canny(gray,50,150)	# Tlow=50, Thigh=150으로 설정
-canny2=cv.Canny(gray,100,200)	# Tlow=100, Thigh=200으로 설정
+canny1 = cv.Canny(gray, 50, 150)  # Tlow=50, Thigh=150으로 설정
+canny2 = cv.Canny(gray, 100, 200)  # Tlow=100, Thigh=200으로 설정
 
-cv.imshow('Original',gray)
-cv.imshow('Canny1',canny1)
-cv.imshow('Canny2',canny2)
+cv.imshow('Original', gray)
+cv.imshow('Canny1', canny1)
+cv.imshow('Canny2', canny2)
 
 cv.waitKey()
 cv.destroyAllWindows()
--- ch4/4-4.py
+++ ch4/4-4.py
@@ -1,14 +1,15 @@
-import cv2 as cv 
+import cv2 as cv
 
-img=cv.imread('apples.jpg')
-gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+# 허프 변환 - 원 검출
+img = cv.imread('apples.jpg')
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)  # 그레이 영상
 
-apples=cv.HoughCircles(gray,cv.HOUGH_GRADIENT,1,200,param1=150,param2=20,minRadius=50,maxRadius=120)
+apples = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, 200, param1=150, param2=20, minRadius=50, maxRadius=120)
 
-for i in apples[0]: 
-    cv.circle(img,(int(i[0]),int(i[1])),int(i[2]),(255,0,0),2)
+for i in apples[0]:
+    cv.circle(img, (int(i[0]), int(i[1])), int(i[2]), (255, 0, 0), 2)
 
-cv.imshow('Apple detection',img)  
+cv.imshow('Apple detection', img)
 
 cv.waitKey()
 cv.destroyAllWindows()
