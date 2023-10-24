7e4090474d01cbf913ed1cb6db07fdbc7335340b
twosky0202 <leehaneul0202@naver.com>
2023-10-24 16:37:43 +0900

    1017 connectedComponents, moments

--- ch4/4-8.py
+++ ch4/4-8.py
@@ -2,35 +2,39 @@ import skimage
 import numpy as np
 import cv2 as cv
 
-orig=skimage.data.horse()
-img=255-np.uint8(orig)*255
-cv.imshow('Horse',img)
+orig = skimage.data.horse()  # 배경 1, 물체 0(black)
+img = 255 - np.uint8(orig) * 255  # 0/1 -> 0/255 -> 255/0 (255에서 빼주기)
+cv.imshow('Horse', img)
 
-contours,hierarchy=cv.findContours(img,cv.RETR_EXTERNAL,cv.CHAIN_APPROX_NONE)
+contours, hierarchy = cv.findContours(img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
 
-img2=cv.cvtColor(img,cv.COLOR_GRAY2BGR)		# 컬러 디스플레이용 영상
-cv.drawContours(img2,contours,-1,(255,0,255),2)
-cv.imshow('Horse with contour',img2)
+img2 = cv.cvtColor(img, cv.COLOR_GRAY2BGR)  # 컬러 디스플레이용 영상
+cv.drawContours(img2, contours, -1, (255, 0, 255), 2)
+cv.imshow('Horse with contour', img2)
 
-contour=contours[0]
+contour = contours[0]
 
-m=cv.moments(contour)				# 몇 가지 특징 
-area=cv.contourArea(contour)
-cx,cy=m['m10']/m['m00'],m['m01']/m['m00']
-perimeter=cv.arcLength(contour,True)
-roundness=(4.0*np.pi*area)/(perimeter*perimeter)
-print('면적=',area,'\n중점=(',cx,',',cy,')','\n둘레=',perimeter,'\n둥근 정도=',roundness)
+m = cv.moments(contour)  # 몇 가지 특징
+print(m)
 
-img3=cv.cvtColor(img,cv.COLOR_GRAY2BGR)		# 컬러 디스플레이용 영상
+area = cv.contourArea(contour)
+cx, cy = m['m10'] / m['m00'], m['m01'] / m['m00']
+perimeter = cv.arcLength(contour, True)
+roundness = (4.0 * np.pi * area) / (perimeter * perimeter)
+print('면적=', area, '\n중점=(', cx, ',', cy, ')', '\n둘레=', perimeter, '\n둥근 정도=', roundness)
 
-contour_approx=cv.approxPolyDP(contour,8,True)	# 직선 근사
-cv.drawContours(img3,[contour_approx],-1,(0,255,0),2)
+img3 = cv.cvtColor(img, cv.COLOR_GRAY2BGR)  # 컬러 디스플레이용 영상
 
-hull=cv.convexHull(contour)			# 볼록 헐
-hull=hull.reshape(1,hull.shape[0],hull.shape[2])
-cv.drawContours(img3,hull,-1,(0,0,255),2)
+contour_approx = cv.approxPolyDP(contour, 8, True)  # 직선 근사. 픽셀의 개수가 적을수록 일반화된 도형
+cv.drawContours(img3, [contour_approx], -1, (0, 255, 0), 2)
+contour_approx = cv.approxPolyDP(contour, 20, True)  # 직선 근사. 숫자가 크면 클수록 선택된 화소의 개수가 적음 -> 모양이 더 대략적인 결과
+cv.drawContours(img3, [contour_approx], -1, (255, 255, 0), 2)
 
-cv.imshow('Horse with line segments and convex hull',img3)
+hull = cv.convexHull(contour)  # 볼록 헐. 볼록 다각형. 하나도 움푹 들어가지 않게끔
+rehull = hull.reshape(1, hull.shape[0], hull.shape[2])  # 행렬에 대한 형태를 바꿈
+cv.drawContours(img3, rehull, -1, (0, 0, 255), 2)
+
+cv.imshow('Horse with line segments and convex hull', img3)
 
 cv.waitKey()
 cv.destroyAllWindows()
